b88dbf18de5fad917e4780c34c342e3e6d36944a
skyer <sky13net84@gmail.com>
2026-02-03 01:33:06 +0300

    соединение устройств (ветки)2

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,9 @@ public class Main {
 
         System.out.println("Задание 5\n");
 
-        System.out.println("Задание 7\n");
-
         System.out.println("Задание 6\n");
+
+        System.out.println("Задание 7\n");
 
         System.out.println("Задание 8\n");
     }
