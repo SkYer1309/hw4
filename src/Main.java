2867ab8d3146bb4eba7c4a4cd7e43ff53a136c5e
skyer <sky13net84@gmail.com>
2026-02-02 21:58:10 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание 1");
+        System.out.println("Задание 1\n");
+        byte age = 5;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
         }
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершенноллетия \n" +
+                    "нужно немного подождать \n");
+        }
+        System.out.println("Задание 2");
     }
+}
