760d469597ad6c6ae2c85b64191d676351f99d7b
admin <skyer13@mail.ru>
2026-02-03 01:15:55 +0300

    6

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание 1");
+        System.out.println("Задание 1\n");
+        byte age = 12;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
         }
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершенноллетия \n" +
+                    "нужно немного подождать \n");
+        }
+        System.out.println("Задание 2\n");
+
+        System.out.println("Задание 3\n");
+
+        System.out.println("Задание 4\n");
+
+        System.out.println("Задание 5\n");
+
+        System.out.println("Задание 6\n");
+
+
     }
+}
